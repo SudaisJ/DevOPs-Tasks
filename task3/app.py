ba2002d4a0ab810494c60ad761c406f9590658a0
Sudais <sudaisj435@gmail.com>
2025-12-19 17:52:44 +0500

    Added docker-compose ...

--- task3/app.py
+++ task3/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
-import os
+from redis import Redis
 
 app = Flask(__name__)
+# Connect to the Redis container named 'redis'
+redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-    return "Hello! This app is running inside a Docker container."
+    count = redis.incr('hits')
+    return f"Hello! You have viewed this page {count} times."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
